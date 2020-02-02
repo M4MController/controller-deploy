2727b5a9fee706714257ea5e838bc2c2d18d8ad5
Ed Asriyan <ed-asriyan@protonmail.com>
2020-02-02 21:34:45 +0300

    Create hold-usb-devices.sh

---
+++ hold-usb-devices.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# https://unix.stackexchange.com/a/183492
+
+echo \
+'SUBSYSTEM=="tty", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", SYMLINK+="obd"
+SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", SYMLINK+="xbee"
+' > /etc/udev/rules.d/99-usb-serial.rules;
+
+udevadm trigger;
