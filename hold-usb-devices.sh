0fdcfea91ddad7afc41f5ab825dd0443260a90d5
Ed Asriyan <ed-asriyan@protonmail.com>
2020-03-05 19:58:30 +0300

    Add the second xbee

--- hold-usb-devices.sh
+++ hold-usb-devices.sh
@@ -5,6 +5,7 @@
 echo \
 'SUBSYSTEM=="tty", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", SYMLINK+="obd"
 SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", SYMLINK+="xbee"
+SUBSYSTEM=="tty", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0001", SYMLINK+="xbee"
 ' > /etc/udev/rules.d/99-usb-serial.rules;
 
 udevadm trigger;
