aa1b85d4520917ac1a4cc404e661101ba1eaa56c
Ed Asriyan <ed-asriyan@protonmail.com>
2020-05-14 16:17:08 +0300

    Add docker-compose installation

--- install_docker.sh
+++ install_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-curl -sSL https://get.docker.com | sh
-
+curl -sSL https://get.docker.com | sh && \
+  apt install -y libffi-dev libssl-dev python3 python3-pip && apt remove python-configparser && \
+  pip3 install docker-compose;
