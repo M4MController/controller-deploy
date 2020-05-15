4b52740b45b204576113b576ecf4f3a60a4e3fb2
Ed Asriyan <ed-asriyan@protonmail.com>
2020-05-15 15:49:09 +0300

    Prune after up

--- update-controller.sh
+++ update-controller.sh
@@ -1,3 +1,4 @@
 curl https://raw.githubusercontent.com/M4MController/controller-deploy/master/docker-compose.prod.yaml -o /tmp/docker-compose.yaml && \
   docker-compose -f /tmp/docker-compose.yaml pull && \
-  docker-compose -f /tmp/docker-compose.yaml up -d;
+  docker-compose -f /tmp/docker-compose.yaml up -d && \
+  docker system prune -a --volumes;
