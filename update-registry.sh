fb53fb2bc73f4421b9273e5cb846ce5d9c727bef
Ed Asriyan <ed-asriyan@protonmail.com>
2020-05-15 14:12:52 +0300

    Do not use buildx

--- update-registry.sh
+++ update-registry.sh
@@ -1,34 +1,37 @@
-# useful links:
-# - buildx:
-#     https://collabnix.com/building-arm-based-docker-images-on-docker-desktop-made-possible-using-buildx/
-# - qemu & docker (skip if you use docker desktop):
-#     https://matchboxdorry.gitbooks.io/matchboxblog/content/blogs/build_and_run_arm_images.html
-# - enable all qemu configurations (skip if you use docker desktop):
-#     https://github.com/qemu/qemu/blob/master/scripts/qemu-binfmt-conf.sh
-
-# at first enable experimental features:
-export DOCKER_CLI_EXPERIMENTAL=enabled
+# at first enable experimental features on docker daemon:
+# /etc/docker/daemon.js
+# {
+#   "experimental": true
+# }
 
 # controller-database
-docker buildx build -t registry.meter4.me:25410/controller-database -f Dockerfile-db --platform linux/arm/v7 --push https://github.com/M4MController/controller-web.git
+docker build -t registry.meter4.me:25410/controller-database -f Dockerfile-db https://github.com/M4MController/controller-web.git
+docker push registry.meter4.me:25410/controller-database
 
 # controller-backend
-docker buildx build -t registry.meter4.me:25410/controller-backend --platform linux/arm/v7 --push https://github.com/M4MController/controller-web.git
+docker build -t registry.meter4.me:25410/controller-backend https://github.com/M4MController/controller-web.git
+docker push registry.meter4.me:25410/controller-backend
 
 # controller-frontend
-docker buildx build -t registry.meter4.me:25410/controller-frontend --platform linux/arm/v7 --build-arg MODE=lite --build-arg GOOGLE_API_MAPS_KEY=$GOOGLE_API_MAPS_KEY --push https://github.com/M4MController/frontend.git
+docker build -t registry.meter4.me:25410/controller-frontend --squash --build-arg MODE=lite --build-arg GOOGLE_API_MAPS_KEY=$GOOGLE_API_MAPS_KEY https://github.com/M4MController/frontend.git
+docker push registry.meter4.me:25410/controller-frontend
 
 # controller-radio
-docker buildx build -t registry.meter4.me:25410/controller-radio --platform linux/arm/v7 --push https://github.com/M4MController/controller-radio.git
+docker build -t registry.meter4.me:25410/controller-radio https://github.com/M4MController/controller-radio.git
+docker push registry.meter4.me:25410/controller-radio
 
 # controller-sync
-docker buildx build -t registry.meter4.me:25410/controller-sync --platform linux/arm/v7 --push https://github.com/M4MController/controller-sync.git
+docker build -t registry.meter4.me:25410/controller-sync https://github.com/M4MController/controller-sync.git
+docker push egistry.meter4.me:25410/controller-sync
 
 # sensor-obd
-docker buildx build -t registry.meter4.me:25410/sensor-obd --platform linux/arm/v7 --push https://github.com/M4MController/sensor-obd.git
+docker build -t registry.meter4.me:25410/sensor-obd https://github.com/M4MController/sensor-obd.git
+docker push registry.meter4.me:25410/sensor-obd
 
 # sensor-gps
-docker buildx build -t registry.meter4.me:25410/sensor-gps --platform linux/arm/v7 --push https://github.com/M4MController/sensor-gps.git
+docker build -t registry.meter4.me:25410/sensor-gps https://github.com/M4MController/sensor-gps.git
+docker push registry.meter4.me:25410/sensor-gps
 
 # sensor-electricity
-docker buildx build -t registry.meter4.me:25410/sensor-electricity --platform linux/arm/v7 --push https://github.com/M4MController/sensor-electricity.git
+docker build -t registry.meter4.me:25410/sensor-electricity https://github.com/M4MController/sensor-electricity.git
+docker push registry.meter4.me:25410/sensor-electricity
